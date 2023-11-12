4661461b151561e1d23a0d6c3d463487653957b5
Daniel Cojocea <danielcojocea28@gmail.com>
2023-11-12 18:07:30 -0500

    added About Me section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Introduction from './components/Introduction';
+import About from './components/About';
 
 
 import './App.css';
@@ -10,6 +11,7 @@ function App() {
     <>
      <Header />
      <Introduction />
+     <About />
      <Footer /> 
     </>
   );
