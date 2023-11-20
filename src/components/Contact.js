1882b8cf4e6a5cffe4a2cdd492cfbd4dc93048e8
Daniel Cojocea <danielcojocea28@gmail.com>
2023-11-20 18:31:40 -0500

    emailJs form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,21 +1,37 @@
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import { BsSendFill } from "react-icons/bs";
 import SectionHeading from "./SectionHeading";
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_ed5dyuc', 'template_ish4sog', form.current, 'waiqH8816mDwvpKL4')
+      .then((result) => {
+          console.log(result.text);
+          e.target.reset();
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+  
   return (
     <div className="pattern">
       <div className="section d-flex justify-content-around align-items-center">
         <div className="contact text-center">
             <SectionHeading head="Contact" title="Get In Touch" />
-            <form action="post" className="d-flex flex-column">
-              <label htmlFor="name-textbox">NAME</label>
-              <input type="text" id="name-textbox" autoComplete="off" />
-              <label htmlFor="email-textbox">EMAIL</label>
-              <input type="email" id="email-textbox" autoComplete="off" />
-              <label htmlFor="message-textbox">MESSAGE</label>
-              <textarea id="message-textbox" rows="5"></textarea>
+            <form ref={form} onSubmit={sendEmail} className="d-flex flex-column">
+              <label>NAME</label>
+              <input type="text" id="name-textbox" name="user_name" autoComplete="off" />
+              <label>EMAIL</label>
+              <input type="email" id="email-textbox" name="user_email" autoComplete="off" />
+              <label>MESSAGE</label>
+              <textarea id="message-textbox" name="message" rows="5"></textarea>
               <div className="d-flex justify-content-end">
-                <button className="send-btn">Hit me up <BsSendFill /></button>
+                <button type="submit" className="send-btn">Hit me up <BsSendFill /></button>
               </div> 
             </form>
                        
