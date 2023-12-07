9912fd0e5c499c1e4237d0074c84a681d672ab8b
Daniel Cojocea <danielcojocea28@gmail.com>
2023-12-07 18:41:52 -0500

    404 page styling

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import '../css/notfound.css';
+import Header from './Header';
+import Footer from './Footer';
+import { HashLink } from 'react-router-hash-link';
+import crack from '../svg/crack.svg'
 
 function NotFound() {
   return (
-    <div>
-      Not Found
-    </div>
+    <>
+      <Header />
+      <div className="not-found d-flex justify-content-center">
+        <img src={crack} alt="svg" />
+        <div className="d-flex flex-column align-items-center align-self-center">
+          <h1>404</h1>
+          <h4 className="tag">page not found</h4>
+          <p>Oops! The page you were looking for doesn't exist.</p>
+          <div className="d-flex flex-column align-items-center align-content-center">
+            <span>Let's get you back ...</span>
+            <HashLink to="/#" className="home-btn text-center curtain">Home</HashLink>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
   )
 }
 
