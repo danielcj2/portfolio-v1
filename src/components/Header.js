8173267eba123b04dab7c7796e1a710aab87c0d9
Daniel Cojocea <danielcojocea28@gmail.com>
2024-01-03 19:07:47 -0500

    fixed hamburger menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -34,13 +34,15 @@ const Header = () => {
           <div className="header-component"><HashLink to="/#contact" scroll={scrollWidthOffset} onClick={showNavBar}>contact</HashLink></div>
           <div className="header-component"><div className={ darkTheme ? "theme-change-btn" : "theme-change-btn moon"} onClick={toggleTheme}>{darkTheme ? <IoSunny /> : <IoMoon />}</div></div>
         </nav>
-        <div className="nav-btn" onClick={showNavBar}>
-          <svg class="bars" viewBox="0 0 100 100" onclick="this.classList.toggle('active')">
-            <path class="line top" d="m 30,33 h 40 c 13.100415,0 14.380204,31.80258 6.899646,33.421777 -24.612039,5.327373 9.016154,-52.337577 -12.75751,-30.563913 l -28.284272,28.284272"></path>
-            <path class="line middle" d="m 70,50 c 0,0 -32.213436,0 -40,0 -7.786564,0 -6.428571,-4.640244 -6.428571,-8.571429 0,-5.895471 6.073743,-11.783399 12.286435,-5.570707 6.212692,6.212692 28.284272,28.284272 28.284272,28.284272"></path>
-            <path class="line bottom" d="m 69.575405,67.073826 h -40 c -13.100415,0 -14.380204,-31.80258 -6.899646,-33.421777 24.612039,-5.327373 -9.016154,52.337577 12.75751,30.563913 l 28.284272,-28.284272"></path>
-          </svg>
-        </div>
+        <input type="checkbox" id="nav-btn" class="nav-btn hidden" />
+        <label for="nav-btn" onClick={showNavBar}>
+            <div class="hamburger">
+                <span class="bar bar1"></span>
+                <span class="bar bar2"></span>
+                <span class="bar bar3"></span>
+                <span class="bar bar4"></span>
+            </div>
+        </label>
       </div>
     </header>
   )
